bcdbbdb26dd00ab11f6507230dad84bcc973324a
Ales <dev.ales.k@gmail.com>
2018-03-12 20:26:47 +0100

    context additional fields

--- manifest.go
+++ manifest.go
@@ -1,15 +1,8 @@
 package pages
 
 type Manifest struct {
-	API string `json:"api"`
-	/*Resources     []*Resource `json:"resources"`
-	Name          string      `json:"name"`*/
-	Dist      string   `json:"dist"`
-	Templates []string `json:"templates"`
-	/*Files         []string    `json:"files"`*/
-	Routers []*Router `json:"routers"`
-	Layout  string    `json:"layout"`
-	/*DefaultLocale string      `json:"default_locale"`*/
+	Imports []string `json:"imports"`
+	Routes  []*Route `json:"routes"`
 }
 
 /*type Resource struct {
@@ -17,24 +10,42 @@ type Manifest struct {
 	Type string `json:"type"`
 }*/
 
-type Router struct {
-	Layout string            `json:"layout"`
-	Handle map[string]*Route `json:"handle"`
-	/*Auth   *Auth             `json:"auth"`*/
-}
-
+/*
+path is a string that uses the route matcher DSL.
+pathMatch is a string that specifies the matching strategy.
+matcher defines a custom strategy for path matching and supersedes path and pathMatch.
+component is a component type.
+redirectTo is the url fragment which will replace the current matched segment.
+outlet is the name of the outlet the component should be placed into.
+canActivate is an array of DI tokens used to look up CanActivate handlers. See CanActivate for more info.
+canActivateChild is an array of DI tokens used to look up CanActivateChild handlers. See CanActivateChild for more info.
+canDeactivate is an array of DI tokens used to look up CanDeactivate handlers. See CanDeactivate for more info.
+canLoad is an array of DI tokens used to look up CanLoad handlers. See CanLoad for more info.
+data is additional data provided to the component via ActivatedRoute.
+resolve is a map of DI tokens used to look up data resolvers. See Resolve for more info.
+runGuardsAndResolvers defines when guards and resolvers will be run. By default they run only when the matrix parameters of the route change. When set to paramsOrQueryParamsChange they will also run when query params change. And when set to always, they will run every time.
+children is an array of child route definitions.
+loadChildren is a reference to lazy loaded child routes. See LoadChildren for more info.
+ */
 type Route struct {
-	locale      string
-	pattern     string
-	layout      string
-	Layout      string      `json:"layout"`
-	Page        string      `json:"page"`
-	Alternative Alternative `json:"alternative"`
+	Path      string   `json:"path"`
+	Component string   `json:"component"`
+	Outlet    string   `json:"outlet"`
+	Children  []*Route `json:"children"`
+
+	CanActivate      interface{} `json:"canActivate"`      // not implemented
+	CanActivateChild interface{} `json:"canActivateChild"` // not implemented
+	PathMatch        interface{} `json:"pathMatch"`        // not implemented
+	RedirectTo       interface{} `json:"redirectTo"`       // not implemented
+
+	parents []*Route
 }
 
+/*
 type Alternative map[string]string
 
 type Auth struct {
 	Login     string `json:"login"`
 	SignInURL string `json:"sign_in_url"`
 }
+*/
--- pages.go
+++ pages.go
@@ -1,27 +1,25 @@
 package pages
 
 import (
-	"html/template"
 	"path"
 	"path/filepath"
 	"github.com/gorilla/mux"
 	"bytes"
-	"fmt"
 	"net/http"
+	"io/ioutil"
+	"github.com/cbroglie/mustache"
+	"github.com/PuerkitoBio/goquery"
 	"errors"
-	"io"
+	"regexp"
 )
 
 type Pages struct {
 	*mux.Router
 	*Options
 	*Manifest
-	TemplateFilePaths []string
-	templates         *template.Template
-	translations      NamedTranslations
-	functions         template.FuncMap
-
-	currentContext *Context
+	partials  mustache.StaticProvider
+	templates map[string]*mustache.Template
+	documents map[string]*goquery.Document
 }
 
 type Options struct {
@@ -30,12 +28,17 @@ type Options struct {
 	JsonFilePath string
 }
 
+var (
+	DefaultOutlet = "#outlet"
+)
+
 func New(opt *Options) (*Pages, error) {
 	p := &Pages{
-		Options:      opt,
-		Router:       mux.NewRouter(),
-		Manifest:     new(Manifest),
-		translations: NamedTranslations{},
+		Options:   opt,
+		Router:    mux.NewRouter(),
+		Manifest:  new(Manifest),
+		templates: map[string]*mustache.Template{},
+		documents: map[string]*goquery.Document{},
 	}
 
 	// read manifest
@@ -47,109 +50,200 @@ func New(opt *Options) (*Pages, error) {
 	// set base path from calling script absolute path and settings.json dir
 	p.base = path.Dir(p.JsonFilePath)
 
-	// set compile path
-	if !path.IsAbs(p.Dist) {
-		p.Dist = path.Join(p.base, p.Dist)
-	}
-
-	// set template functions
-	if p.Options.IsRendering {
-		p.functions = renderingTemplateFunctions(p)
-	} else {
-		p.functions = templateFunctions(p)
-	}
-
 	// parse templates
-	for _, templatesPath := range p.Templates {
+	var partials = map[string]string{}
+	for _, templatesPath := range p.Imports {
 		if !path.IsAbs(templatesPath) {
 			templatesPath = path.Join(p.base, templatesPath)
 		}
 		fs, err := filepath.Glob(templatesPath)
 		if err != nil {
-			panic(err)
+			return p, err
 		}
-		p.TemplateFilePaths = append(p.TemplateFilePaths, fs...)
+
+		// read templates and load into map
+
+		for _, f := range fs {
+			templateBytes, err := ioutil.ReadFile(f)
+			if err != nil {
+				return p, err
+			}
+			name := path.Base(f)
+			name = name[0 : len(name)-len(path.Ext(name))]
+			content := string(templateBytes)
+
+			partials[name] = content
+		}
 	}
-	if len(p.TemplateFilePaths) > 0 {
-		p.templates, err = template.New("").Funcs(p.functions).ParseFiles(p.TemplateFilePaths...)
+
+	p.partials = mustache.StaticProvider{Partials: partials}
+
+	// parse partials and replace {{>partial}} with template
+	// is this even needed here?
+	for name, part := range partials {
+		p.templates[name], err = mustache.ParseStringPartials(part, &p.partials)
 		if err != nil {
 			return p, err
 		}
-	} else {
-		return p, errors.New("templates undefined")
+	}
+
+	for name, temp := range p.partials.Partials {
+		buf := new(bytes.Buffer)
+		buf.WriteString(temp)
+		doc, err := goquery.NewDocumentFromReader(buf)
+		if err != nil {
+			return p, err
+		}
+		p.documents[name] = doc
 	}
 
 	return p, nil
 }
 
-func (p *Pages) Execute(wr io.Writer, layout string, page string) error {
-	p.currentContext = &Context{
-		/*data: map[string]interface{}{},*/
-		Page: page,
+
+//todo: 1
+func iter(h map[string][]*Route, route *Route, basePath string, parents []*Route) map[string][]*Route {
+	route.parents = parents
+
+	if entry, ok := h[basePath+route.Path]; ok {
+		entry = append(entry, route)
+	} else {
+		h[basePath+route.Path] = []*Route{route}
 	}
-	buf := new(bytes.Buffer)
-	err := p.templates.ExecuteTemplate(buf, page, p.currentContext)
+
+	if len(route.Children) > 0 {
+		ps := append(parents, route)
+		for _, childRoute := range route.Children {
+			h = iter(h, childRoute, basePath+route.Path, ps)
+		}
+	}
+	return h
+}
+
+func (p *Pages) BuildRouter() (err error) {
+	p.Router = mux.NewRouter()
+
+	// attaches routes to paths - this way we don't have two Handlers for the same path
+
+	var handle map[string][]*Route
+	for _, route := range p.Routes {
+		handle = iter(map[string][]*Route{}, route, "",nil)
+	}
+
+	for path, routes := range handle {
+		err = p.handleRoute(p.Router, path, routes)
+		if err != nil {
+			return err
+		}
+	}
+
+	return err
+}
+
+// one path can have multiple routes defined -> when having multiple routers on one page
+func (p *Pages) handleRoute(r *mux.Router, path string, routes []*Route) (err error) {
+	//mux.NewRouter().PathPrefix(opt.HandlerPathPrefix).Subrouter(),
+
+	if len(route.Children) > 0 {
+		ps := append(parents, route)
+		for _, childRoute := range route.Children {
+			err = p.buildRoute(r, childRoute, basePath+route.Path, ps)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	err = p.handle(r, route, basePath, parents)
 	if err != nil {
 		return err
 	}
-	p.currentContext.html = template.HTML(buf.String())
-	return p.templates.ExecuteTemplate(wr, layout, p.currentContext)
+
+	return err
 }
 
-func (p *Pages) BuildRouter() {
-	var layout string
-	for _, router := range p.Routers {
-		layout = p.Layout
-		if len(router.Layout) > 0 {
-			layout = router.Layout
-		}
-		for pattern, route := range router.Handle {
-			if len(route.Layout) == 0 {
-				route.Layout = layout
-			}
-			route.pattern = pattern
+// also needs to pass parent routes that need to be rendered before any child
+func (p *Pages) buildRoute(r *mux.Router, route *Route, basePath string, parents []*Route) (err error) {
+	//mux.NewRouter().PathPrefix(opt.HandlerPathPrefix).Subrouter(),
 
-			/*if route.Alternative != nil {
-				route.Alternative[p.DefaultLocale] = pattern
-			}*/
-
-			p.handle(pattern, route)
-
-			for locale, pattern := range route.Alternative {
-				p.handle(pattern, &Route{
-					locale:      locale,
-					pattern:     pattern,
-					Alternative: route.Alternative,
-					Page:        route.Page,
-					Layout:      route.Layout,
-					layout:      route.layout,
-				})
+	if len(route.Children) > 0 {
+		ps := append(parents, route)
+		for _, childRoute := range route.Children {
+			err = p.buildRoute(r, childRoute, basePath+route.Path, ps)
+			if err != nil {
+				return err
 			}
 		}
 	}
+
+	err = p.handle(r, route, basePath, parents)
+	if err != nil {
+		return err
+	}
+
+	return err
 }
 
-func (p *Pages) handle(pattern string, route *Route) {
-	p.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		ctx := &Context{
-			/*data: map[string]interface{}{},*/
-			Page: route.Page,
-			Vars: vars,
-		}
-		p.currentContext = ctx
-		buf := new(bytes.Buffer)
-		err := p.templates.ExecuteTemplate(buf, route.Page, ctx)
-		if err != nil {
-			panic(err)
-		}
-		ctx.html = template.HTML(buf.String())
+func (p *Pages) handle(router *mux.Router, r *Route, basePath string, parents []*Route) error {
+	// 1. Render parents first
+	// 2. Render actual route
 
-		buf2 := new(bytes.Buffer)
-		err = p.templates.ExecuteTemplate(buf2, route.Layout, ctx)
-		if err != nil {
-			fmt.Fprint(w, err)
+	var document = p.documents["index"].Clone()
+
+	for _, parent := range append(parents, r) {
+		// set outlet
+		outlet := parent.Outlet
+		if len(outlet) == 0 {
+			outlet = DefaultOutlet
 		}
-		w.Write(buf2.Bytes())
+
+		outletSelection := document.Find(outlet)
+		if outletSelection.Length() == 0 {
+			return errors.New("can't find router outlet " + outlet)
+		}
+
+		component := p.documents[parent.Component].Clone()
+		if component.Children().Length() == 0 {
+			return errors.New("component empty " + parent.Component)
+		}
+
+		componentHtml, err := component.Html()
+		if err != nil {
+			return err
+		}
+
+		outletSelection.SetHtml(componentHtml)
+	}
+
+	html, err := document.Html()
+	if err != nil {
+		return err
+	}
+
+	html = regexp.MustCompile(`{{\s*(&gt;)`).ReplaceAllString(html, "{{>")
+
+	temp, err := mustache.ParseStringPartials(html, &p.partials)
+	if err != nil {
+		return err
+	}
+
+	router.HandleFunc(basePath+r.Path, func(w http.ResponseWriter, req *http.Request) {
+		//vars := mux.Vars(r)
+
+		temp.FRender(w, map[string]interface{}{
+			"page": r.Component,
+		})
+		//w.Write([]byte(html))
 	})
+
+	/*router.PathPrefix(basePath+r.Path).HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		//vars := mux.Vars(r)
+
+		temp.FRender(w, map[string]interface{}{
+			"page": r.Component,
+		})
+		//w.Write([]byte(html))
+	})*/
+
+	return nil
 }
